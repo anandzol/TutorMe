5005ea030f54aab58f523a27a74f4ec6b3945f56
Yashica <ge37goj@mytum.de>
2021-07-25 17:32:47 +0200

    fix - on edit home refresh

--- src/frontend/src/components/HomeScreen.js
+++ src/frontend/src/components/HomeScreen.js
@@ -53,9 +53,9 @@ const defaultState = {
 class HomeScreen extends Component {
     constructor() {
         super();
+        // window.location.reload()
         const currentUserJWT = AuthService.getCurrentUser();
         const currentUser = parseJwt(currentUserJWT);
-        console.log(currentUser);
         if (currentUser === undefined) {
             return;
         }
@@ -70,6 +70,9 @@ class HomeScreen extends Component {
                 this.setState({ university: response.data.university });
             }
         });
+        console.log("props", this.props)
+        // if (this.props?.location?.state?.firstName)
+        //         this.setState({firstName: this.props.location.state.firstName})
     }
 
     render() {
@@ -86,10 +89,13 @@ class HomeScreen extends Component {
                 </Link>
             );
         }
+
         return (
+            
             <div>
                 <div className={classes.firstName}>
-                    <h1>{`Welcome ${this.state.firstName}!`}</h1>
+                    {console.log("inside render",this.props)}
+                    <h1>{`Welcome ${this.props?.location?.state.firstName ? this.props.location.state.firstName : this.state.firstName}!`}</h1>
                 </div>
                 <div className={classes.center}>
                     <img className={classes.image} src={tutorLogo} />
@@ -98,7 +104,7 @@ class HomeScreen extends Component {
                     <h2 className={classes.subText}>Your Personalised, Tailored Tutoring</h2>
                     <h2 className={classes.subText}>See available learnings for your university</h2>
                     {link}
-                </div>
+            </div>
             </div>
         );
     }
