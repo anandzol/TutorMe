25e56982a049c5cdb36cd6d020096a79bff02e49
Yashica <ge37goj@mytum.de>
2021-07-25 19:56:24 +0200

    style: homescreen

--- src/backend/routes/api/fileUpload.js
+++ src/backend/routes/api/fileUpload.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 // Multer adds a body object and a file or files object to the request object. The body object contains the values of the text fields of the form, the file or files object contains the files uploaded via the form.
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
-const fs = require('fs');
+// const fs = require('fs');
 
 const Document = require('../../models/document');
 
