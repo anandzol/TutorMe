aa8b05ea5232a174900cd30895efe60541a11eaf
Minh Tran <minh.tran@tum.de>
2021-07-25 09:15:14 +0200

    feat: add footer styling

--- src/frontend/src/components/Footer.js
+++ src/frontend/src/components/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import CopyrightIcon from '@material-ui/icons/Copyright';
 import { MDBContainer } from 'mdbreact';
-function Footer() {
+
+const useStyles = makeStyles(theme => ({
+    footer: {
+        backgroundColor: '#cad8ee'
+    }
+}));
+
+function Footer(props) {
+    const classes = useStyles();
+
     return (
-        <div className="footer-copyright text-center py-3">
+        <div className={`footer-copyright text-center py-3 ${classes.footer}`}>
             <MDBContainer fluid>
                 &copy; {new Date().getFullYear()} Copyright:{' '}
                 <a href="https://github.com/Minh13/TutorMe"> TutorMe</a>
